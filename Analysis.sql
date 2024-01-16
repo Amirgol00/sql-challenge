1ba3d371148af97441d73a60b23af9d19d51116e
Amirgol00 <amir.golshan.al@gmail.com>
2024-01-15 23:23:30 -0500

    Update Analysis.sql

--- Analysis.sql
+++ Analysis.sql
@@ -25,6 +25,25 @@ SELECT employees.first_name, employees.last_name, employees.sex
 FROM employees
 WHERE employees.first_name = 'Hercules' AND employees.last_name LIKE 'B%';
 
+--List each employee in the Sales department, including their employee number, last name, and first name
+SELECT employees.emp_no, employees.last_name, employees.first_name
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales';
 
+--List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name IN ('Sales', 'Development');
+
+
+--List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+SELECT employees.last_name, COUNT(*) as frequency
+FROM employees
+GROUP BY employees.last_name
+ORDER BY frequency DESC;
 
 
